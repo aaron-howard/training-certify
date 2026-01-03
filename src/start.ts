a5ffa3eab439dceb5b8eead3ca4130b3abdc9867
Aaron Howard <aaron.howard@dallas.gov>
2026-01-02 22:55:27 -0600

    Remove start.ts - Clerk middleware not compatible with current TanStack Start version

--- src/start.ts
+++
@@ -1,6 +0,0 @@
-import { createStart } from '@tanstack/react-start'
-import { clerkMiddleware } from '@clerk/tanstack-react-start/server'
-
-export default createStart({
-    requestMiddleware: [clerkMiddleware()],
-})
